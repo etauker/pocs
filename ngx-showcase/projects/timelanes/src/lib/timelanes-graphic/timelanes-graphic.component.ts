dcdb7dc3e3e663f0f0abadb18b349ef18806fc76
Tautvydas Kersulis <etauker@gmail.com>
2021-06-06 01:08:15 +0100

    Added mock data service

--- ngx-showcase/projects/timelanes/src/lib/timelanes-graphic/timelanes-graphic.component.ts
+++ ngx-showcase/projects/timelanes/src/lib/timelanes-graphic/timelanes-graphic.component.ts
@@ -4,6 +4,7 @@ const moment = moment_;
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { TimelanesGraphic } from '../classes/timelanes-graphic';
 import { Period } from '../model/period.interface';
+import { MockDataService } from '../mock-data/mock-data.service';
 
 @Component({
   selector: 'lib-timelanes-graphic',
@@ -12,9 +13,11 @@ import { Period } from '../model/period.interface';
 })
 export class TimelanesGraphicComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  constructor(private mockDataService: MockDataService) { }
 
   ngAfterViewInit() {
+
+    // needed to calculate percent-based SVG height and width
     setTimeout(() => this.init(), 10);
   }
   
@@ -26,61 +29,7 @@ export class TimelanesGraphicComponent implements OnInit, AfterViewInit {
 
 
     // format data
-    const PERIODS: Period[] = [
-      {
-          start: moment('08-03-2021', 'DD-MM-YYYY').subtract(16, 'hours').valueOf(),
-          end: moment('08-03-2021', 'DD-MM-YYYY').subtract(495, 'minutes').valueOf(),
-          style: {
-              backgroundColour: 'grey',
-              fillColour: 'black',
-              fillStyle: 'none',
-          },
-          annotation1: { text: moment('08-03-2021', 'DD-MM-YYYY').format('dddd') },
-          annotation2: { text: '7.75h' }
-      },
-      {
-          start: moment('08-03-2021', 'DD-MM-YYYY').subtract(8, 'hours').valueOf(),
-          end: moment('08-03-2021', 'DD-MM-YYYY').subtract(3, 'hours').valueOf(),
-          style: {
-              backgroundColour: 'yellow',
-              fillColour: 'orange',
-              fillStyle: 'line-solid',
-          },
-          annotation1: { text: moment('08-03-2021', 'DD-MM-YYYY').format('dddd') },
-          annotation2: { text: '5h' }
-      },
-      {
-          start: moment('08-03-2021', 'DD-MM-YYYY').subtract(3, 'hours').valueOf(),
-          end: moment('08-03-2021', 'DD-MM-YYYY').subtract(2, 'hours').valueOf(),
-          style: {
-              backgroundColour: 'yellow',
-              fillColour: 'red',
-              fillStyle: 'line-solid',
-          },
-          annotation1: { text: moment('08-03-2021', 'DD-MM-YYYY').format('dddd') },
-          annotation2: { text: '1h' }
-      },
-      {
-          start: moment('08-03-2021', 'DD-MM-YYYY').add(2, 'days').subtract(7, 'hours').valueOf(),
-          end: moment('08-03-2021', 'DD-MM-YYYY').add(2, 'days').subtract(3, 'hours').valueOf(),
-          style: { fillStyle: 'none' },
-          annotation1: {
-              text: moment('08-03-2021', 'DD-MM-YYYY').add(2, 'days').format('dddd'),
-              textColor: 'green',
-          },
-          annotation2: { text: '4h' }
-      },
-      {
-          start: moment('08-03-2021', 'DD-MM-YYYY').add(6, 'days').subtract(20, 'hours').valueOf(),
-          end: moment('08-03-2021', 'DD-MM-YYYY').add(6, 'days').subtract(12, 'hours').valueOf(),
-          style: {
-              fillStyle: 'line-dashed',
-              fillColour: 'orange',
-          },
-          annotation1: { text: moment('08-03-2021', 'DD-MM-YYYY').add(6, 'days').format('dddd') },
-          annotation2: { text: '8h' }
-      }
-    ];
+    const PERIODS: Period[] = this.mockDataService.getMockPeriods();
 
     const timeline = new TimelanesGraphic({
       svg: 'visual',
--- ngx-showcase/projects/timelanes/src/lib/timelanes.module.ts
+++ ngx-showcase/projects/timelanes/src/lib/timelanes.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { TimelanesComponent } from './timelanes/timelanes.component';
 import { TimelanesGraphicComponent } from './timelanes-graphic/timelanes-graphic.component';
+import { MockDataService } from './mock-data/mock-data.service';
 
 @NgModule({
   imports: [
   ],
   declarations: [TimelanesComponent, TimelanesGraphicComponent],
-  exports: [TimelanesComponent]
+  exports: [TimelanesComponent],
+  providers: [MockDataService]
 })
 export class TimelanesModule { }
